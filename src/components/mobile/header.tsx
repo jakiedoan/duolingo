2583aea0d9ab084c6a7f6008669167c520795058
Jakie Doan <jakiedoan@gmail.com>
2024-07-12 18:00:47 +0700

    MobileHeader modified.

--- src/components/mobile/header.tsx
+++ src/components/mobile/header.tsx
@@ -1,7 +1,12 @@
-import { UserInfo } from '@/utils/types';
+import {
+  UserCourse,
+  UserInfo,
+  UserProgress as UserProgressType,
+} from '@/utils/types';
 import React from 'react';
 import UserProgress from '@/components/user-progress';
 import { TFunction } from 'i18next';
+import { useFetchQuery, usePutQuery } from '@/services';
 
 type Props = {
   user: UserInfo;
@@ -10,20 +15,45 @@ type Props = {
 };
 
 function MobileHeader({ user, t, lng }: Props) {
+  const {
+    isPending,
+    error,
+    mutateAsync: edit,
+  } = usePutQuery('update_progress', `progress/${user?.id}`, true);
+
+  const { data: progress } = useFetchQuery<UserProgressType>(
+    'user_progress',
+    `user/${user?.id}/progress`,
+    true
+  );
+
+  const { data: courses } = useFetchQuery<UserCourse[]>(
+    'user_courses',
+    `user/${user?.id}/courses`,
+    true
+  );
+
+  const handleSelectCourse = (id: string) => {
+    edit({
+      activeCourseId: id,
+    });
+  };
+
   return (
     <nav className='px-6 h-[50px] flex items-center bg-snow fixed top-0 w-full z-50'>
-      {user.progress && (
+      {progress && courses && (
         <UserProgress
-          courses={user.courses}
+          courses={courses}
           activeCourse={{
-            title: t(`course.${user.progress.active_course.title}`),
-            imageSrc: user.progress.active_course.image_src,
+            title: t(`course.${progress.active_course.title}`),
+            imageSrc: progress.active_course.image_src,
           }}
-          streak={user.progress.streak}
-          gems={user.progress.gems}
-          hearts={user.progress.hearts}
+          streak={progress.streak}
+          gems={progress.gems}
+          hearts={progress.hearts}
           hasActiveSubscription={false}
           lng={lng}
+          handleChangeCourse={handleSelectCourse}
         />
       )}
     </nav>
